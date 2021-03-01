58b2d2aabb8b93bfc9cc9fd7650c32f74f1ea96c
Jabba Laci <jabba.laci@gmail.com>
2021-03-01 15:41:55 +0100

    4. ea.

---
+++ eloadasok/04_fuggvenyek_tombok/sources/kiir.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello\n");
+
+    puts("world");
+
+    putchar('A');
+    putchar('B');
+    putchar('B');
+    putchar('A');
+    putchar('\n');
+
+    puts("");
+
+    printf("__END__");
+
+    return 0;
+}
---
+++ eloadasok/04_fuggvenyek_tombok/sources/mem_szemet1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+
+    // Hiba! Nem adtunk
+    // neki kezdőértéket!
+    printf("%d\n", x);
+
+    return 0;
+}
