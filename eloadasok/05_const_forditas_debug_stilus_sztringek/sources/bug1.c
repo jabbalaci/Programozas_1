f5244ef77d4eb0c706a182bbd4742678788ceea9
Jabba Laci <jabba.laci@gmail.com>
2021-03-09 07:25:35 +0100

    5. ea.

---
+++ eloadasok/05_const_forditas_debug_stilus_sztringek/sources/bug1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    string s = get_string("Mi a neved?\n");
+
+    printf("Hello %s!\n", s);
+
+    return 0;
+}
---
+++ eloadasok/05_const_forditas_debug_stilus_sztringek/sources/bug2c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Írassuk ki a '*' karaktert 5-ször.
+// Használjunk debugger-t.
+
+int main()
+{
+    for (int i = 0; i <= 5; ++i)
+    {
+        printf("*\n");
+    }
+
+    return 0;
+}
