440b87bd0ed4edf055ff9fd80f55b97e5088bf49
Jabba Laci <jabba.laci@gmail.com>
2023-03-13 11:13:07 +0100

    5. ea.

---
+++ eloadasok/05_const_forditas_debug_stilus_sztringek/sources/bug2a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Írassuk ki a '*' karaktert 5-ször.
+
+int main()
+{
+    for (int i = 0; i <= 5; ++i)
+    {
+        printf("*\n");
+    }
+
+    return 0;
+}
---
+++ eloadasok/05_const_forditas_debug_stilus_sztringek/sources/bug2b.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Írassuk ki a '*' karaktert 5-ször.
+
+int main()
+{
+    for (int i = 0; i <= 5; ++i)
+    {
+        printf("# i értéke: %d\n", i);
+        printf("*\n");
+    }
+
+    return 0;
+}
