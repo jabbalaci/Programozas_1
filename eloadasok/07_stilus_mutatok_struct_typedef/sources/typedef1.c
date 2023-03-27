d46f0d9e675fe00603f97e92b8394e2f7688678e
Jabba Laci <jabba.laci@gmail.com>
2023-03-27 17:14:48 +0200

    L

---
+++ eloadasok/07_stilus_mutatok_struct_typedef/sources/typedef1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+
+    A C nyelvet ne magyarosítsuk! Ez csak egy példa.
+
+*/
+
+typedef int egesz;
+
+int main()
+{
+    int a = 5;
+    egesz b = 9;
+
+    printf("%d\n", a);    // 5
+    printf("%d\n", b);    // 9
+
+    return 0;
+}
