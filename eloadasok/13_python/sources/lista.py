f6a7fd33716493ecd28343a30232fcd297684413
Jabba Laci <jabba.laci@gmail.com>
2024-05-14 05:58:44 +0200

    13. ea.

---
+++ eloadasok/13_python/sources/lista.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def main():
+    li = [1, 2, 3, 4, 5, 6, 7, 8]
+    paros = []
+
+    for n in li:
+        if n % 2 == 0:
+            paros.append(n)
+        #
+    #
+    print(paros)
+
+##############################################################################
+
+if __name__ == "__main__":
+    main()
