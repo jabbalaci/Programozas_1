8b4b6029d136ddf7bc2e5447eba6f99d135e8987
Jabba Laci <jabba.laci@gmail.com>
2025-04-25 15:55:55 +0200

    9. ea.

---
+++ eloadasok/09_memoria_stack_heap/sources/stack_overflow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+const int BYTE = 1;
+const int KB = 1024 * BYTE;
+const int MB = 1024 * KB;
+
+int main()
+{
+    int x, y;
+    double d;
+    // char tomb[7 * MB];
+    char tomb[9 * MB];
+
+    puts("END");
+
+    return 0;
+}
