b9197cb46e43f786ff038f856414bb0232fb4cfe
Jabba Laci <jabba.laci@gmail.com>
2026-05-11 13:27:56 +0200

    13. ea.

---
+++ eloadasok/13_python/sources/hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# megjegyzés
+
+def main():
+    print("hello")
+    print("world")
+    if True:
+        print("na most ez")
+        print("nagyon igaz")
+    # endif
+    print("END")
+# enddef
+
+
+main()
